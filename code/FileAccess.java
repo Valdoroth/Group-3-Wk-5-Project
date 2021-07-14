7b20b3c43177c48532889108e9ac4993bbbc1946
Valdoroth <hofflerstephen@gmail.com>
2021-07-14 10:13:09 -0400

    setBooks done. Takes in a List<Books> so edit list and then write to file.

--- code/FileAccess.java
+++ code/FileAccess.java
@@ -28,37 +28,43 @@ public class FileAccess {
 
     public static void setBooks(List<Books> booksUpdated) throws IOException {
         bookList = booksUpdated;
-        BufferedWriter writeToFile = new BufferedWriter(new FileWriter(String.valueOf(bookLocation2)));
+        FileWriter writeToFile = new FileWriter(String.valueOf(bookLocation2));
         //writeToFile.write(String.valueOf(bookList));
-        for (Books line : bookList) writeToFile.write(String.valueOf(line));
-
+        StringBuilder lineToWrite = new StringBuilder();
+        for (Books line : bookList) {
+            lineToWrite.append(line.getISBN()).append(",/").append(line.getTitle()).append(",/").append(line.getAuthor()).append(",/").append(line.getCheckOutQty()).append(",/").append(line.getTotalStock()).append(",/").append(line.getPrice()).append(",/").append(line.getDescription()).append("\n");
+            System.out.println(lineToWrite);
+            writeToFile.write(String.valueOf(lineToWrite));
+        }
     }
 
     public static void main(String[] args) throws IOException {
 
         List<Books> bookList = getBooks();
         //String lookMeUp = "978-0865165601";
-        String[] manyISBN = {"978-0865165601","978-1250252715","978-1250142283"};
+        //String[] manyISBN = {"978-0865165601","978-1250252715","978-1250142283"};
         int stockChange;
-        /*
-        for (Books i : bookList) {
-            System.out.println("ISBN: " + i.getISBN() + "\tTitle: " + i.getTitle());
-        }
-        */
+
+        //for (Books i : bookList) {
+        //    System.out.println("ISBN: " + i.getISBN() + "\tTitle: " + i.getTitle());
+        //}
+
         HashMap<Integer,Books> bookMap = new HashMap<>();
         for (Books k : bookList) bookMap.put(bookList.indexOf(k),k);
-//        for(int p = 0; p < 10 ; p++) System.out.println(bookMap.get(p).getPrice());
+        for(int p = 0; p < 10 ; p++) {
+            System.out.println(bookMap.get(p).getPrice());
+        }
         bookMap.get(0).checkOutBook();
 
         System.out.println("=============================");
 
-        for (Books i : bookList) {
-            for (String j : manyISBN) {
-                if (i.getISBN().equals(j))
-                    System.out.println("Title: " + i.getTitle() + "\tAvailable Amount: " + i.getAvailableAmount());// System.out.println("ISBN: " + i.getISBN() + "\tTitle: " + i.getTitle());
+        //for (Books i : bookList) {
+        //    for (String j : manyISBN) {
+        //        if (i.getISBN().equals(j))
+        //            System.out.println("Title: " + i.getTitle() + "\tAvailable Amount: " + i.getAvailableAmount());// System.out.println("ISBN: " + i.getISBN() + "\tTitle: " + i.getTitle());
                 //else System.out.println("Book is not in library.");
-            }
-        }
+         //   }
+        //}
 
 
         //setBooks(bookList);
